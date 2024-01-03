989f2eb0d47fd61cf06a3461d2c283488b40220e
Smoothex <momchildp@gmail.com>
2024-01-03 13:39:20 +0100

    remove default ALLOWED_HOSTS val from settings.py

--- Routing_Api/Routing_Api/settings.py
+++ Routing_Api/Routing_Api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = BUSNOW_ENVIRONMENT == 'DEBUGGING'
 
 # Who can access the server? ['localhost', '127.0.0.1'] for local debugging
-ALLOWED_HOSTS = (os.environ.get('ALLOWED_HOSTS', ['*']))
+ALLOWED_HOSTS = (os.environ.get('ALLOWED_HOSTS'))
 
 
 # Application definition
