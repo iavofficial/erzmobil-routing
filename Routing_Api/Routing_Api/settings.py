70f1d8371eb0be208fd14752da00f80c393a78a5
Smoothex <momchildp@gmail.com>
2024-01-03 13:18:46 +0100

    add django SECRET_KEY to .env and read it from settings.py

--- Routing_Api/Routing_Api/settings.py
+++ Routing_Api/Routing_Api/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = BUSNOW_ENVIRONMENT == 'DEBUGGING'
